f712c9b8fea38f1b0b9b81625798328aa8379621
NKisser <nkisser@student.tgm.ac.at>
2024-01-21 14:17:39 +0100

    Fixing projectsturcture

--- src/PersistencyService.java
+++
@@ -1,8 +0,0 @@
-import org.json.simple.JSONObject;
-
-public interface PersistencyService {
-
-    public void save(String path, int rounds, int score, String url, String word);
-
-    public JSONObject read(String path);
-}
